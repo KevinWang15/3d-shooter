2452c9b519157ec72d1271adc1b834e55b25188e
Ke Wang <453587854@qq.com>
2017-12-11 16:52:57 +0800

    去掉y轴方向的相机旋转(算法太复杂)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import PointerLockControls from "./lib/PointerLockControls";
 import * as THREE from "three";
 import * as models from "./models";
 import setUpEnvironment, { meshes } from "./methods/setUpEnvironment";
@@ -6,7 +5,6 @@ import "./index.css";
 
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(75, 1000 / 800, 0.1, 100);
-let controls = new PointerLockControls(camera);
 let loadingManager = new THREE.LoadingManager();
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(1000, 800);
@@ -15,7 +13,6 @@ renderer.shadowMap.type = THREE.BasicShadowMap;
 models.load(loadingManager).then(function () {
   setUpEnvironment(scene);
   document.body.appendChild(renderer.domElement);
-  scene.add(controls.getObject());
   camera.lookAt(meshes["tree1"].position);
   camera.position.y += 2;
   console.log(camera.rotation);
@@ -23,6 +20,24 @@ models.load(loadingManager).then(function () {
     renderer.domElement.requestPointerLock()
   };
   animate();
+});
+
+document.addEventListener('mousemove', () => {
+  camera.rotation.y += event.movementX * 0.002;
+  if (meshes.ak) {
+    let time = Date.now() * 0.0005;
+    meshes.ak.position.set(
+      camera.position.x - Math.sin(camera.rotation.y + Math.PI / 6) * 0.75,
+      camera.position.y - 0.5 + Math.sin(time * 4 + camera.position.x + camera.position.z) * 0.05,
+      camera.position.z + Math.cos(camera.rotation.y + Math.PI / 6) * 0.75,
+    );
+
+    meshes.ak.rotation.set(
+      camera.rotation.x,
+      camera.rotation.y - Math.PI,
+      camera.rotation.z,
+    );
+  }
 });
 
 let prevTime = performance.now();
